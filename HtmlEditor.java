0ed7392956b88f633adf92c9d73c188241015430
Josaphat Valdivia <josaphat.valdivia@gmail.com>
2012-09-17 01:01:20 -0400

    Starts the GUI

--- HtmlEditor.java
+++ HtmlEditor.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * TODO: docs
  *
@@ -5,5 +8,13 @@
 public class HtmlEditor {
 	public static void main(String[] args){
 		// TODO
+		String[] constructs = {
+				"b", "i",
+				"table", "tr", "td",
+				"h1", "h2", "h3", "h4", "h5", "h6",
+				"ol", "ul", "li",
+				"dl", "dt", "dd",
+		};
+		new gui.EditorWindow(new ArrayList<String>(Arrays.asList(constructs)));
 	}
 }
