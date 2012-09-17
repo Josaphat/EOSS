349952086aa5edbdf25a3e0885324e0af55cb8f7
Josaphat Valdivia <josaphat.valdivia@gmail.com>
2012-09-17 01:54:14 -0400

    DocumentManger touched

---
+++ main/DocumentManager.java
@@ -0,0 +1,18 @@
+package main;
+
+public class DocumentManager {
+	
+	/**
+	 * TODO: Docs
+	 */
+	public void open(){
+		// TODO
+	}
+	
+	/**
+	 * TODO: Docs
+	 */
+	public void save(){
+		// TODO
+	}
+}
