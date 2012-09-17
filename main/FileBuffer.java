bd52a76c882b5142996dbb24c5d3d9ef2f605de7
Josaphat Valdivia <josaphat.valdivia@gmail.com>
2012-09-17 02:13:02 -0400

    Outlined the FileBuffer class

--- main/FileBuffer.java
+++ main/FileBuffer.java
@@ -1,15 +1,33 @@
 package main;
+
+import java.util.ArrayList;
+
 /**
- * The FileBuffer is a collection of Lines. The lines know whether
- * or not they're line-wrapped
+ * FileBuffer contains the contents of a file
  * @author jxv1308
  *
  */
 public class FileBuffer {
-	private java.util.ArrayList<String> lines;
 	private Validator validator;
+	private ArrayList<String> lines;
 	
-	public boolean validate(){
+	public FileBuffer(Validator validator) {
+		this.validator = validator;
+	}
+	
+	/**
+	 * TODO: Docs
+	 * @return true if the file is valid, false if it is not
+	 */
+	public boolean validate() {
 		return validator.validate();
+	}
+
+	/**
+	 * Adds the given line after the last line of the buffer.
+	 * @param line String to append in a new line
+	 */
+	public void append(String line){
+		this.lines.add(line);
 	}
 }
